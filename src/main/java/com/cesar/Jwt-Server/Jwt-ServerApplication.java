f004fdede417ca9542100c6947fcb002dff8afcb
CsarNarciso <cesarpazol1029@gmail.com>
2025-02-01 17:47:03 -0600

    Pre load users with roles and permissions using commandlinerunner

--- src/main/java/com/cesar/Jwt-Server/Jwt-ServerApplication.java
+++ src/main/java/com/cesar/Jwt-Server/Jwt-ServerApplication.java
@@ -1,4 +1,4 @@
-package com.cesar.Backend;
+package com.cesar.Jwt-Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,4 +10,77 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner init(){
+		
+		return args -> {
+			//Create and pre-load test entities on DB
+			
+			//PERMISSIONS
+			Set<String> permissionNames = Set.of("READ", "WRITE", "DELETE", "REFACTOR");
+			List<PermissionEntity> permissions = new ArrayList<>();
+			
+			//For each permission namen
+			permissionsNames.forEach(name -> {
+				
+				//create new entity
+				PermissionEntity permission = PermissionEntity
+								.builder()
+									.name(name)
+								.build();
+				permissions.add(permission);
+			});
+			
+			//ROLES
+			List<RoleEntity> roles = new ArrayList<>();
+			String[][] rolePermissions = {
+				{"READ"},
+				{"READ", "WRITE"},
+				{"READ", "WRITE", "DELETE"},
+				{"READ", "WRITE", "DELETE", "REFACTOR"}
+			}
+			
+			//For each role name,
+			for(int i = 0; i < RoleEnum.values(); i++){
+				
+				//create new entity
+				RoleEntity role = RoleEntity
+							.builder()
+								.name(RoleEnum.values().get(i))
+								.permissions(rolePermissions[i].asSet())
+							.build();
+				roles.add(role);
+			});
+			
+			//USERS
+			Set<String> usernames = Set.of("Juan12", "Maria54", "PedroGarcia");
+			String[][] userRoles = {
+				{RoleEnum.USER, RoleEnum.GUEST},
+				{RoleEnum.ADMIN},
+				{RoleEnum.DEVELOPER}
+			};
+			List<UserEntity> users = new ArrayList<>();
+			
+			//For each username
+			for(int i = 0; i < usernames.length(); i++){
+				
+				//create new entity
+				UserEntity user = UserEntity
+							.builder()
+									.username(usernames.get(i))
+									.password("changeme")
+									.isEnabled(true)
+									.isAccountNoExpired(false)
+									.isAccountNoLocked(false)
+									.isCredentialNoExpired(false)
+									.roles(userRoles[i].asSet())
+								.build();
+				users.add(user);			
+			}
+			
+			
+			//Save on DB
+			userRepo.saveAll(users);
+		}
+	}
 }
