fd909f99afe1ea5192b97692684d4a5fd3b9b0b9
CsarNarciso <cesarpazol1029@gmail.com>
2025-07-14 15:59:09 -0600

    Consume server auth login, and resource endpoints

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const baseUrl = 'http://localhost:9090';
+
+  const login = () => {
+    fetch(`${baseUrl}/auth/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: "user",
+        password: "password"
+      })
+    });
+  }
+
+  const getResource = () => {
+    fetch(`${baseUrl}/resource`, {
+      credentials: 'include',
+      method: 'GET'
+    });
+  }
+
+  const postResource = () => {
+    fetch(`${baseUrl}/resource`, {
+      credentials: 'include',
+      method: 'POST'
+    });
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={login}>Login</button>
+        <button onClick={getResource}>GET</button>
+        <button onClick={postResource}>POST</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
